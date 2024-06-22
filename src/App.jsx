f100e6b22761fae6ddfeb72c077da8f8c061d54f
kiwifruit47 <victoria.dimitrovax@icloud.com>
2024-06-23 02:06:34 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import './App.css';
+import { FiPlusCircle } from "react-icons/fi";
+import { MdOutlineDoneOutline } from "react-icons/md";
+
+function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+  const [toBeDeleted, setToBeDeleted] = useState(null);
+
+  const deleteTask = (taskValue) => {
+    setToBeDeleted(taskValue);
+    setTimeout(() => {
+      setTodoList(todoList.filter(task => task != taskValue));
+      setToBeDeleted(null);
+    }, 1000)
+  }
+
+  return (
+    <div className='container'>
+      <h1>Todo List</h1>
+      <div className='taskAddingContainer'>
+        <input 
+          type="text" 
+          name="newTaskInput" 
+          placeholder='Add new task'
+          value={newTask} 
+          onChange={(event) => {
+            event.preventDefault();
+            setNewTask(event.target.value);
+        }}/>
+        <button onClick={() => {
+          if (newTask != "") {
+            setTodoList([...todoList, newTask])
+            setNewTask("");
+          } else {
+            alert("No task in text field");
+          }
+          
+          }}>
+            <FiPlusCircle style={{height: "2rem", width: "2rem"}}/>
+        </button>
+      </div>
+      
+      <div className='todoList'>
+        {todoList.map((taskValue, key) => {
+          return <div key={index++} style={{backgroundColor: toBeDeleted === taskValue ? "#96C291" : "#D2649A"}} className='taskBox'>
+            {id}. {taskValue}
+            <button  className='doneBtn' onClick={() => deleteTask(taskValue)}>
+              <MdOutlineDoneOutline style={{height: "2rem", width: "2rem", backgroundColor: toBeDeleted === taskValue ? "#96C291" : "#D2649A"}}/>
+            </button>
+            </div>
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default App
